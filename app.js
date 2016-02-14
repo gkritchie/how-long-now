4addffdfcb98edc508e1a629c3597b3e0686df12
Gray <gkritchie@gmail.com>
2016-02-14 11:50:22 +1100

    Add support for multiple events

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-var jen = {
-    evName: "Jenna Arrives!!",
-    evDate: "2016-03-10T08:50:00+11:00",
-};
+var events = [
+    {
+        evName: "Jenna Arrives!!",
+        evDate: "2016-03-10T08:50:00+11:00",
+    },
+    {
+        evName: "Mum and Dad here",
+        evDate: "2016-03-04T17:00:00+11:00"
+    },
+    {
+        evName: "Coming Home!",
+        evDate: "2016-03-24T15:30:00+13:00"
+    }
+];
 
 function getTimeRemaining(eventTime) {
     var t = Date.parse(eventTime) - Date.now();
@@ -19,6 +29,8 @@ function getTimeRemaining(eventTime) {
     };
 }
 
+var currentInterval = null;
+
 function initializeClock(id, eventTime) {
     var clock = document.getElementById(id);
 
@@ -28,6 +40,9 @@ function initializeClock(id, eventTime) {
     var secsSpan = clock.querySelector('.seconds');
 
     function updateClock() {
+        if (currentInterval && currentInterval != timeInterval) {
+            clearInterval(currentInterval);
+        }
         var t = getTimeRemaining(eventTime);
 
         daysSpan.innerHTML = ('0' + t.days).slice(-2);
@@ -42,4 +57,22 @@ function initializeClock(id, eventTime) {
 
     updateClock();
     var timeInterval = setInterval(updateClock, 1000);
+    currentInterval = timeInterval;
+}
+
+function initializeEvents(id) {
+    var evList = document.getElementById(id);
+
+    events.forEach(function(ev, index, arr) {
+        var newEl = document.createElement('li');
+        newEl.className = 'eventLink';
+        newEl.innerHTML = ev.evName;
+        newEl.onclick = function() {
+            initializeClock('clockdiv', ev.evDate);
+        };
+        evList.appendChild(newEl);
+    });
+
+    var firstEl = evList.firstElementChild;
+    firstEl.onclick().apply(firstEl);
 }
